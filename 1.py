ca34ca78a21e91db9ead1e2a1498897effd19c06
Anastasiya-Garina <114314685+Anastasiya-Garina@users.noreply.github.com>
2022-10-06 00:47:17 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,8 @@
+a = "Nastya"
+b = "Garina"
+c = 2004
+print(a, "_", b, "_", c, sep = "")
+
+a, b = b, a
+c += 60
+print(a, "_", b, "_", c, sep = "")
---
+++ 2.py
@@ -0,0 +1,5 @@
+a_katet = 3
+b_katet = 4
+c_gipotenyza = 5
+print("площадь:", a_katet * b_katet // 2)
+print("периметр:", a_katet + b_katet + c_gipotenyza)
