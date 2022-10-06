342f1e5ec200ecebcac2d4f8eb6735a9665c5d2c
Anastasiya-Garina <114314685+Anastasiya-Garina@users.noreply.github.com>
2022-10-06 22:00:40 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+###1
 a = "Nastya"
 b = "Garina"
 c = 2004
@@ -6,3 +7,15 @@ print(a, "_", b, "_", c, sep = "")
 a, b = b, a
 c += 60
 print(a, "_", b, "_", c, sep = "")
+print("")
+
+###2
+a = str(input("Введите имя: "))
+b = str(input("Введите фамилию: "))
+c = int(input("Введите год: "))
+print(a, "_", b, "_", c, sep = "")
+
+a, b = b, a
+c += 60
+print(a, "_", b, "_", c, sep = "")
+print("")
